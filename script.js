b9576756893020f2ff69329d41d620123253e53a
MuscleQuant <2023uee2016@mnit.ac.in>
2026-06-04 18:41:12 +0530

    finished the project, some UI subtle changes can be done, some bugs can be removed.

--- script.js
+++ script.js
@@ -37,6 +37,8 @@ function gameController(player1,player2,currentPlayer){
 
     let board=gameboard.getBoard();
     let gameOver=false;
+    let winner=null;
+    let tie=null;
 
 
      const winningPatterns=[ [0,1,2],
@@ -50,7 +52,7 @@ function gameController(player1,player2,currentPlayer){
                         ]
 
     function tieDetection(){
-        if(!checkWinner() && !(board.includes(`_`))){
+        if(!winner && !(board.includes(`_`))){
             return true;
         }
         return false;
@@ -59,6 +61,8 @@ function gameController(player1,player2,currentPlayer){
      function resetGame(){
         currentPlayer=player1;
         gameOver=false;
+        winner=null;
+        tie=null;
         gameboard.reset();
         console.error(`Resettting Game`);
         
@@ -85,7 +89,8 @@ function gameController(player1,player2,currentPlayer){
 
         if (gameOver){
             console.log(`game over`);
-            resetGame();
+            // stop it for the meantime
+            //resetGame();
             return ;
         }
 
@@ -95,8 +100,8 @@ function gameController(player1,player2,currentPlayer){
         }
         console.log(`The current Player is : `,currentPlayer.playerName);
         gameboard.placeMarker(position,currentPlayer.marker);
-        const winner=checkWinner();
-        const tie=tieDetection();
+        winner=checkWinner();
+        tie=tieDetection();
         if (tie){
             console.log(`its a tie babe`);
             gameOver=true;
@@ -120,10 +125,18 @@ function gameController(player1,player2,currentPlayer){
     function getCurrentPlayer(){
         return currentPlayer;
     }
+    function getWinner(){
+        return winner;
+    }
+    function isGameOver(){
+        return gameOver;
+    }
+    function isTie(){
+        return tie;
+    }
 
 
-
-    return {playRound,getCurrentPlayer}
+    return {playRound,getCurrentPlayer,getWinner,isGameOver,isTie,resetGame}
  
 }
 
@@ -133,19 +146,107 @@ const game=gameController(player1,player2,player1);
 
 
 
-game.playRound(0);
-game.playRound(7);
-game.playRound(1);
-game.playRound(3);
-game.playRound(2);
-game.playRound(4);
+ //game.playRound(0);
+// game.playRound(7);
+// game.playRound(1);
+// game.playRound(3);
+// game.playRound(2);
+// game.playRound(4);
 
 
 
-
-
-// TODO:
-
-// - Add tie detection
-// - Connect winner detection to game flow
 // - Build DOM display controller
+
+const displayController = (()=>{
+
+    const cellNodeList=document.querySelectorAll(".cell");
+    const currentPlayerDisplay=document.querySelector('#currentPlayerDisplay');
+    const winningPlayerDisplay=document.querySelector('#winningPlayerDisplay');
+    const tieDisplay=document.querySelector('#tieDisplay');
+    const winnerName=document.querySelector(".winnerName");
+    const restartButton=document.createElement("button");
+    restartButton.addEventListener("click",()=>{
+        resetGameUI();
+    })
+
+    function displayPlayerChoice(i){
+        game.playRound(i);
+    }
+
+        cellNodeList.forEach((cell,index) =>{
+            cell.addEventListener("click",()=>{
+                displayPlayerChoice(index);
+                renderBoard();
+                renderStatus();
+                
+            })
+        })
+    function renderBoard(){
+        const sampleBoard=gameboard.getBoard();
+        sampleBoard.forEach((value,index)=>{
+            cellNodeList[index].textContent=value;
+        })
+    }
+    
+    function renderRestartButton(){
+        if (game.getWinner() || game.isTie()){
+            restartButton.textContent="Restart Game";
+            restartButton.style.fontSize=`1em`;
+            restartButton.style.display="block";
+            winnerName.appendChild(restartButton);
+        }
+    }
+    function resetGameUI(){
+            game.resetGame();
+            renderBoard();
+            // renderStatus(); not needed now
+            winningPlayerDisplay.textContent = "";
+            tieDisplay.textContent = "";
+
+            currentPlayerDisplay.textContent =`New game! ${game.getCurrentPlayer().playerName} starts`;
+            
+            restartButton.style.display="none";
+    }
+
+    // function renderStatus(){
+    //     currentPlayerDisplay.textContent=`The Current Player is: ${game.getCurrentPlayer().playerName}`
+    //     if (game.getWinner()){
+    //         winningPlayerDisplay.textContent=`The Winner is : ${game.getWinner().playerName}`
+    //         currentPlayerDisplay.textContent=``;
+    //         renderRestartButton();
+    //     }
+    //     else winningPlayerDisplay.textContent=``;
+
+    //     if (game.isTie()){
+    //         tieDisplay.textContent=`The Game is Tie!!`
+    //         currentPlayerDisplay.textContent=``;
+    //         renderRestartButton();
+    //     }
+    //     else tieDisplay.textContent=``;
+        
+    // }
+
+     function renderStatus(){
+        if (game.getWinner()){
+        currentPlayerDisplay.textContent = ``;
+        winningPlayerDisplay.textContent =
+            `The Winner is : ${game.getWinner().playerName}`;
+        renderRestartButton();
+        }
+        else if (game.isTie()){
+        currentPlayerDisplay.textContent = ``;
+        tieDisplay.textContent = `The Game is Tie!!`;
+        renderRestartButton();
+        }
+        else{
+            currentPlayerDisplay.textContent =`The Current Player is: ${game.getCurrentPlayer().playerName}`;
+            winningPlayerDisplay.textContent = ``;
+            tieDisplay.textContent = ``;
+        }
+    }
+
+    return {renderBoard,renderStatus}; 
+})();
+
+displayController.renderBoard();
+displayController.renderStatus()
